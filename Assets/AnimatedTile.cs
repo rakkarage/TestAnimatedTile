84e130e8414ca5072edd3b5a7f89f44fd56cc92e
Rakka Rage <rakkarage@gmail.com>
2018-10-07 14:25:32 -0400

    fixed - use 0 to 1 range for start time

--- Assets/AnimatedTile.cs
+++ Assets/AnimatedTile.cs
@@ -35,9 +35,10 @@ namespace UnityEngine.Tilemaps
 		{
 			if (m_AnimatedSprites.Length > 0)
 			{
+				var total = m_AnimatedSprites.Length * tileAnimationData.animationSpeed;
 				tileAnimationData.animatedSprites = m_AnimatedSprites;
 				tileAnimationData.animationSpeed = Random.Range(m_MinSpeed, m_MaxSpeed);
-				tileAnimationData.animationStartTime = RandomStartTime ? GetRandomFrame() * tileAnimationData.animationSpeed : 0;
+				tileAnimationData.animationStartTime = RandomStartTime ? (GetRandomFrame() * tileAnimationData.animationSpeed) / total : 0;
 				return true;
 			}
 			return false;
@@ -48,54 +49,54 @@ namespace UnityEngine.Tilemaps
 		}
 	}
 
-// #if UNITY_EDITOR
-// 	[CustomEditor(typeof(AnimatedTile))]
-// 	public class AnimatedTileEditor : Editor
-// 	{
-// 		private AnimatedTile tile { get { return (target as AnimatedTile); } }
+	// #if UNITY_EDITOR
+	// 	[CustomEditor(typeof(AnimatedTile))]
+	// 	public class AnimatedTileEditor : Editor
+	// 	{
+	// 		private AnimatedTile tile { get { return (target as AnimatedTile); } }
 
-// 		public override void OnInspectorGUI()
-// 		{
-// 			EditorGUI.BeginChangeCheck();
-// 			int count = EditorGUILayout.DelayedIntField("Number of Animated Sprites", tile.m_AnimatedSprites != null ? tile.m_AnimatedSprites.Length : 0);
-// 			if (count < 0)
-// 				count = 0;
+	// 		public override void OnInspectorGUI()
+	// 		{
+	// 			EditorGUI.BeginChangeCheck();
+	// 			int count = EditorGUILayout.DelayedIntField("Number of Animated Sprites", tile.m_AnimatedSprites != null ? tile.m_AnimatedSprites.Length : 0);
+	// 			if (count < 0)
+	// 				count = 0;
 
-// 			if (tile.m_AnimatedSprites == null || tile.m_AnimatedSprites.Length != count)
-// 			{
-// 				Array.Resize<Sprite>(ref tile.m_AnimatedSprites, count);
-// 			}
+	// 			if (tile.m_AnimatedSprites == null || tile.m_AnimatedSprites.Length != count)
+	// 			{
+	// 				Array.Resize<Sprite>(ref tile.m_AnimatedSprites, count);
+	// 			}
 
-// 			if (count == 0)
-// 				return;
+	// 			if (count == 0)
+	// 				return;
 
-// 			EditorGUILayout.LabelField("Place sprites shown based on the order of animation.");
-// 			EditorGUILayout.Space();
+	// 			EditorGUILayout.LabelField("Place sprites shown based on the order of animation.");
+	// 			EditorGUILayout.Space();
 
-// 			for (int i = 0; i < count; i++)
-// 			{
-// 				tile.m_AnimatedSprites[i] = (Sprite)EditorGUILayout.ObjectField("Sprite " + (i + 1), tile.m_AnimatedSprites[i], typeof(Sprite), false, null);
-// 			}
+	// 			for (int i = 0; i < count; i++)
+	// 			{
+	// 				tile.m_AnimatedSprites[i] = (Sprite)EditorGUILayout.ObjectField("Sprite " + (i + 1), tile.m_AnimatedSprites[i], typeof(Sprite), false, null);
+	// 			}
 
-// 			float minSpeed = EditorGUILayout.FloatField("Minimum Speed", tile.m_MinSpeed);
-// 			float maxSpeed = EditorGUILayout.FloatField("Maximum Speed", tile.m_MaxSpeed);
-// 			if (minSpeed < 0.0f)
-// 				minSpeed = 0.0f;
+	// 			float minSpeed = EditorGUILayout.FloatField("Minimum Speed", tile.m_MinSpeed);
+	// 			float maxSpeed = EditorGUILayout.FloatField("Maximum Speed", tile.m_MaxSpeed);
+	// 			if (minSpeed < 0.0f)
+	// 				minSpeed = 0.0f;
 
-// 			if (maxSpeed < 0.0f)
-// 				maxSpeed = 0.0f;
+	// 			if (maxSpeed < 0.0f)
+	// 				maxSpeed = 0.0f;
 
-// 			if (maxSpeed < minSpeed)
-// 				maxSpeed = minSpeed;
+	// 			if (maxSpeed < minSpeed)
+	// 				maxSpeed = minSpeed;
 
-// 			tile.m_MinSpeed = minSpeed;
-// 			tile.m_MaxSpeed = maxSpeed;
+	// 			tile.m_MinSpeed = minSpeed;
+	// 			tile.m_MaxSpeed = maxSpeed;
 
-// 			tile.m_AnimationStartTime = EditorGUILayout.FloatField("Start Time", tile.m_AnimationStartTime);
-// 			tile.m_TileColliderType = (Tile.ColliderType)EditorGUILayout.EnumPopup("Collider Type", tile.m_TileColliderType);
-// 			if (EditorGUI.EndChangeCheck())
-// 				EditorUtility.SetDirty(tile);
-// 		}
-// 	}
-// #endif
+	// 			tile.m_AnimationStartTime = EditorGUILayout.FloatField("Start Time", tile.m_AnimationStartTime);
+	// 			tile.m_TileColliderType = (Tile.ColliderType)EditorGUILayout.EnumPopup("Collider Type", tile.m_TileColliderType);
+	// 			if (EditorGUI.EndChangeCheck())
+	// 				EditorUtility.SetDirty(tile);
+	// 		}
+	// 	}
+	// #endif
 }
